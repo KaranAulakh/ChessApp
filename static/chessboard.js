91aba9f77f90faabd8592f0868dc1c9b7de52ff0
KaranAulakh <Karansaulakh@gmail.com>
2024-05-10 18:21:25 -0700

    cleanup chessboard.js

--- static/chessboard.js
+++ static/chessboard.js
@@ -1,3 +1,10 @@
+const DARK_SQUARE_COLOR = "#948979"
+const LIGHT_SQUARE_COLOR = "#DFD0B8"
+const POSSIBLE_MOVES_HIGHLIGHT_COLOR = "rgba(21, 52, 72, 0.15)"
+const LIGHT_SQUARE_HIGHLIGHT_COLOR = "#E6E6FA"
+const DARK_SQUARE_HIGHLIGHT_COLOR = "#A7D4CD"
+
+
 Vue.component('chess-board', {
     template: `<canvas ref='canvas' width='512' height='512'></canvas>`,
 
@@ -5,7 +12,7 @@ Vue.component('chess-board', {
         return {
             images: {},
             position: {},
-            highlightedSquares: [],
+            selectedSquare: null,
             possibleMoves: [],
             whiteToMove: true,
         };
@@ -13,15 +20,130 @@ Vue.component('chess-board', {
     async mounted() {
         await this.loadImages();
         await this.fetchPiecePositions("start");
+
+        // handle events for click and window resize
         this.$refs.canvas.addEventListener('click', this.handleClick);
-        window.addEventListener('resize', this.handleResize); // Listen for window resize events
-        this.handleResize(); // Call handleResize initially to ensure proper scaling
+        window.addEventListener('resize', this.handleResize);
+        this.drawChessboard();
     },
     beforeDestroy() {
         this.$refs.canvas.removeEventListener('click', this.handleClick);
-        window.removeEventListener('resize', this.handleResize); // Remove event listener on component destroy
+        window.removeEventListener('resize', this.handleResize);
     },
     methods: {
+        /*
+         * EVENT HANDLING METHODS
+         */
+        async handleClick(event) {
+            const canvas = this.$refs.canvas;
+            const rect = canvas.getBoundingClientRect();
+            const x = Math.floor((event.clientX - rect.left) / 64);
+            const y = Math.floor((event.clientY - rect.top) / 64);
+            const square = this.position[x.toString() + y.toString()];
+
+            // If the clicked square has a piece that belongs to the team who's turn it is, then show options
+            if (!!square && square.includes(this.whiteToMove ? "Light" : "Dark")) {
+                await this.fetchPossibleMoves(x.toString() + y.toString());
+                this.selectedSquare = x.toString() + y.toString();
+            } else {
+                this.possibleMoves = [];
+                this.selectedSquare = null;   
+            }
+
+            this.drawChessboard()
+        },
+        handleResize() {
+            // Redraw the chessboard, will probably need to send in new dimensions
+            this.drawChessboard();
+        },
+        /*
+         * GRAPHIC RENDERING METHODS
+         */
+        async drawChessboard() {
+            const canvas = this.$refs.canvas;
+            const context = canvas.getContext('2d');
+
+            // draw chess board
+            let light = true;
+            for (let x = 0; x < 8; x++) {
+                for (let y = 0; y < 8; y++) {
+                    const color = light ? LIGHT_SQUARE_COLOR : DARK_SQUARE_COLOR;
+                    context.fillStyle = color;
+                    context.fillRect(x * 64, y * 64, 64, 64);
+                    light = !light;
+                }
+                light = !light;
+            }
+
+            this.highlightSquares(context)
+            this.drawPieces(context)   
+        },
+        
+        drawPieces(context) {
+            for (const key in this.position) {
+                context.drawImage(this.images[this.position[key]], key[0] * 64, key[1] * 64, 64, 64);
+            }
+        },
+
+        highlightSquares(context) {
+            // highlight selected square
+            if (!this.selectedSquare) 
+                return;
+            const color = this.selectedSquare[0] % 2 == this.selectedSquare[1] % 2 ? LIGHT_SQUARE_HIGHLIGHT_COLOR : DARK_SQUARE_HIGHLIGHT_COLOR
+            this.drawCircle(context, 0, 0, this.selectedSquare, color)
+
+            // highlight possible moves
+
+            if (!this.possibleMoves) 
+                return;
+            this.possibleMoves.forEach(square => {   
+                this.drawCircle(context, 12, 18, square, POSSIBLE_MOVES_HIGHLIGHT_COLOR)
+            });
+            
+        },
+
+        drawCircle(context, offset, cornerRadius, square, color){
+            const x = parseInt(square[0]) * 64 + offset / 2; 
+            const y = parseInt(square[1]) * 64 + offset / 2;
+        
+            context.fillStyle = color
+            context.beginPath();
+            context.moveTo(x + cornerRadius, y);
+            context.arcTo(x + 64 - offset, y, x + 64 - offset, y + cornerRadius, cornerRadius);
+            context.arcTo(x + 64 - offset, y + 64 - offset, x + 64 - cornerRadius, y + 64 - offset, cornerRadius);
+            context.arcTo(x, y + 64 - offset, x, y + 64 - cornerRadius, cornerRadius);
+            context.arcTo(x, y, x + cornerRadius, y, cornerRadius);
+            context.closePath();
+            context.fill();
+        },
+
+
+        /*
+         * API METHODS
+         */
+        async fetchPiecePositions(move) {
+            try {
+                const response = await fetch(`/get-piece-positions?move=${move}`);
+                this.position = await response.json();
+                // Once positions are fetched, draw the chessboard
+                this.drawChessboard();
+            } catch (error) {
+                console.error('Error fetching piece positions:', error);
+            }
+        },
+        async fetchPossibleMoves(square) {
+            try {
+                const response = await fetch(`/get-possible-moves?square=${square}`);
+                this.possibleMoves = await response.json();
+            } catch (error) {
+                console.error('Error fetching piece positions:', error);
+            }
+        },
+
+
+        /*
+         * SETUP METHODS
+         */
         async loadImages() {
             const imageSources = {
                 "LightPawn": '/static/images/LightPawn.png',
@@ -44,137 +166,6 @@ Vue.component('chess-board', {
                 await img.decode(); // Wait for image to be fully loaded
                 this.images[key] = img;
             }));
-        },
-        async fetchPiecePositions(move) {
-            try {
-                const response = await fetch(`/get-piece-positions?move=${move}`);
-                this.position = await response.json();
-                // Once positions are fetched, draw the chessboard
-                this.drawChessboard();
-            } catch (error) {
-                console.error('Error fetching piece positions:', error);
-            }
-        },
-        async fetchPossibleMoves(square) {
-            try {
-                const response = await fetch(`/get-possible-moves?square=${square}`);
-                this.possibleMoves = await response.json();
-            } catch (error) {
-                console.error('Error fetching piece positions:', error);
-            }
-        },
-        async drawChessboard() {
-            const canvas = this.$refs.canvas;
-            const context = canvas.getContext('2d');
-            const LIGHT_COLOR = "#DFD0B8";
-            const DARK_COLOR = "#948979";
-
-            // draw chess board
-            let light = true;
-            for (let x = 0; x < 8; x++) {
-                for (let y = 0; y < 8; y++) {
-                    const color = light ? LIGHT_COLOR : DARK_COLOR;
-                    context.fillStyle = color;
-                    context.fillRect(x * 64, y * 64, 64, 64);
-                    light = !light;
-                }
-                light = !light;
-            }
-
-
-            this.highlightSquares(context)
-            this.drawPieces(context)
-            
-            
-        },
-        drawPieces(context) {
-            for (const key in this.position) {
-                context.drawImage(this.images[this.position[key]], key[0] * 64, key[1] * 64, 64, 64);
-            }
-        },
-        highlightSquares(context) {
-            const HIGHLIGHT_COLOR = "rgba(21, 52, 72, 0.15)";
-            const cornerRadius = 18;
-            const offset = 8;
-            
-            this.highlightedSquares.forEach(square => {
-                const x = parseInt(square.charAt(0)) * 64 + offset / 2; 
-                const y = parseInt(square.charAt(1)) * 64 + offset / 2;
-            
-                context.fillStyle = HIGHLIGHT_COLOR;
-                context.beginPath();
-                context.moveTo(x + cornerRadius, y);
-                context.arcTo(x + 64 - offset, y, x + 64 - offset, y + cornerRadius, cornerRadius);
-                context.arcTo(x + 64 - offset, y + 64 - offset, x + 64 - cornerRadius, y + 64 - offset, cornerRadius);
-                context.arcTo(x, y + 64 - offset, x, y + 64 - cornerRadius, cornerRadius);
-                context.arcTo(x, y, x + cornerRadius, y, cornerRadius);
-                context.closePath();
-                context.fill();
-            });
-
-            this.possibleMoves.forEach(square => {
-                const x = parseInt(square.charAt(0)) * 64 + offset / 2; 
-                const y = parseInt(square.charAt(1)) * 64 + offset / 2;
-            
-                context.fillStyle = HIGHLIGHT_COLOR;
-                context.beginPath();
-                context.moveTo(x + cornerRadius, y);
-                context.arcTo(x + 64 - offset, y, x + 64 - offset, y + cornerRadius, cornerRadius);
-                context.arcTo(x + 64 - offset, y + 64 - offset, x + 64 - cornerRadius, y + 64 - offset, cornerRadius);
-                context.arcTo(x, y + 64 - offset, x, y + 64 - cornerRadius, cornerRadius);
-                context.arcTo(x, y, x + cornerRadius, y, cornerRadius);
-                context.closePath();
-                context.fill();
-            });
-
-            /* highlight squares with circle
-
-            this.highlightedSquares.forEach(square => {
-                // find center of circle
-                const x = (parseInt(square.charAt(0)) + 0.5) * 64;
-                const y = (parseInt(square.charAt(1)) + 0.5) * 64;
-                const radius = 30; 
-                context.fillStyle = HIGHLIGHT_COLOR;
-                context.beginPath();
-                // start angle = 0 and end angle is 2PI radians (or 360 degrees)
-                context.arc(x, y, radius, 0, 2 * Math.PI);
-                context.fill();
-            });
-            */
-        },
-        async handleClick(event) {
-            this.possibleMoves = []
-            const canvas = this.$refs.canvas;
-            const rect = canvas.getBoundingClientRect();
-            const x = Math.floor((event.clientX - rect.left) / 64);
-            const y = Math.floor((event.clientY - rect.top) / 64);
-            // will clean this up, use !! and a ternary to assign light or dark string
-            if 
-            (this.whiteToMove && 
-             this.position[x.toString() + y.toString()] !== undefined && 
-             this.position[x.toString() + y.toString()].includes("Light")) {
-                this.highlightedSquares = [x.toString() + y.toString()]
-                await this.fetchPossibleMoves(x.toString() + y.toString())
-            } 
-
-            else if 
-            (!this.whiteToMove && 
-             this.position[x.toString() + y.toString()] !== undefined && 
-             this.position[x.toString() + y.toString()].includes("Dark")) {
-                this.highlightedSquares = [x.toString() + y.toString()]
-                await this.fetchPossibleMoves(x.toString() + y.toString())
-            }
-
-            else
-                this.highlightedSquares = [];
-
-            // why is this line not printing each move?
-            console.log(this.possibleMoves)
-            this.drawChessboard()
-        },
-        handleResize() {
-            // Redraw the chessboard, will probably need to seen in new dimensions
-            this.drawChessboard();
         },
     }
 });
