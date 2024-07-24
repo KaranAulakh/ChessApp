f53ffe094f39ebc5a352ae398f756db448b6ab35
KaranAulakh <Karansaulakh@gmail.com>
2024-07-23 22:09:48 -0600

    setup VueApp

---
+++ frontend/frontend/src/components/Chessboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p>{{ board }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "homeView",
+  data() {
+    return {
+      board: "",
+    };
+  },
+  methods: {
+    getResponse() {
+      const path = "http::localhost:5000/play";
+      axios
+        .get(path)
+        .then((res) => {
+          console.log(res.data);
+          this.board = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+  },
+  created() {
+    this.getResponse();
+  },
+};
+</script>
