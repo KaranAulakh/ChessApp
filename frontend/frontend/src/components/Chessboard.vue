bdfc076f4bf125b030287e5fc1ffb04ee97e8ae0
KaranAulakh <Karansaulakh@gmail.com>
2024-07-26 22:34:40 -0600

    style chess board

--- frontend/frontend/src/components/Chessboard.vue
+++ frontend/frontend/src/components/Chessboard.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="chessboard">
-    <div v-for="row in 8" :key="row" class="row">
-      <div
-        v-for="col in 8"
-        :key="col"
-        :class="['cell', getColor(row, col)]"
-      ></div>
+  <div class="chessboard-container">
+    <div class="chessboard">
+      <div v-for="row in 8" :key="row" class="row">
+        <div
+          v-for="col in 8"
+          :key="col"
+          :class="['cell', getColor(row, col)]"
+        ></div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,24 +23,34 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
+.chessboard-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .chessboard {
   display: grid;
   grid-template-rows: repeat(8, 50px);
   grid-template-columns: repeat(8, 50px);
   border: 2px solid #000;
 }
+
 .row {
   display: contents;
 }
+
 .cell {
   width: 50px;
   height: 50px;
 }
+
 .cell.white {
-  background-color: #fff;
+  background-color: #dfd0b8;
 }
+
 .cell.black {
-  background-color: #000;
+  background-color: #948979;
 }
 </style>
--- frontend/frontend/src/components/PlayChess.vue
+++ frontend/frontend/src/components/PlayChess.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="app-container">
     <h1>Welcome to Chess</h1>
     <ChessBoard />
   </div>
@@ -9,9 +9,26 @@
 import ChessBoard from "./ChessBoard.vue";
 
 export default {
-  name: "{PlayChess}",
+  name: "PlayChess",
   components: {
     ChessBoard,
   },
 };
 </script>
+
+<style scoped>
+body,
+html {
+  background-color: #153448;
+  height: 100%;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.app-container {
+  text-align: center;
+  color: white;
+}
+</style>
