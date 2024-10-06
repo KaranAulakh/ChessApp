80f5b12466206bfca1057354edc7b1418ccf9284
Karan Aulakh <karansaulakh@gmail.com>
2024-10-06 16:24:59 -0700

    minor styling updates

--- frontend/frontend/src/components/PlayChess.vue
+++ frontend/frontend/src/components/PlayChess.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="app-container">
-    <h1>Welcome to Chess</h1>
+    <p>Welcome to Chess</p>
     <ChessBoard />
   </div>
 </template>
 
 <script>
-import ChessBoard from "./ChessBoard.vue";
+import ChessBoard from "./Chessboard.vue";
 
 export default {
   name: "PlayChess",
@@ -15,20 +15,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-body,
-html {
-  background-color: #153448;
-  height: 100%;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.app-container {
-  text-align: center;
-  color: white;
-}
-</style>
